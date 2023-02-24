2c8fa37bdbfde388b133e0fb24dff3e93f41137d
noJS <nduytan.uit.a3@gmail.com>
2023-02-24 23:00:31 +0700

    create landing page

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ app.get('/', (req, res) => {
 app.get('/news', (req, res) => {
   res.render('news');
 })
+app.get('/search', (req, res) => {
+  res.render('search');
+})
 
 
 app.listen(port, () => {
